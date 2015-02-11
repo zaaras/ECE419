1f313a67a1fddb49dbe4b4d5426c687308f5ff36
gmumar <gmumar@gmail.com>
2015-02-11 15:19:16 -0500

    bla

---
+++ lab2/mazewar/serverClient.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+
+public class serverClient implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	int x,y;
+	String name;
+}
